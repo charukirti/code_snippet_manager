75841e49c732e3b900b288490509fcf1efeae479
charukirti <charukirticc112002@gmail.com>
2025-11-22 15:56:27 +0530

    removed files

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <SignIn />
-    </div>
-  );
-}
